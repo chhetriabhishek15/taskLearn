adc6e124f36b3dedc2542b4d934b8ccd609a40ea
Abhishek Chhetri <Abhishek.Chhetri@adeccogroup.com>
2024-12-24 18:37:25 +0530

    FIXED SIGNUP

--- signup/views.py
+++ signup/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -10,15 +12,25 @@ def home(request):
 
 
 def signIn(request):
-    if request.method == "POST":
-        DATA = request.POST
-        email = DATA.get("email_Add")
-        passwd = DATA.get("passwd")
-
-        User.objects.create_user(username=email, email=email, password=passwd)
-        return redirect("/signin/")
+    if request.method=="POST":
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        import pdb; pdb.set_trace()
+        if not User.objects.filter(username=email).exists():
+            messages.error(request, 'Invalid Username')
+            return redirect('/signin/')
+        
+        user=authenticate(username=use)
+        
     return render(request, "home/signin.html", context={"page": "SignIn"})
 
 
 def signUp(request):
+    if request.method == "POST":
+        first_name=request.POST.get("first_name")
+        last_name=request.POST.get("last_name")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        User.objects.create_user(username=email,email=email, password=password,first_name=first_name,last_name=last_name)
+        return redirect("/")
     return render(request, "home/signup.html", context={"page": "Signup"})
